313a07824a0a45a6f77e8fc7a833035f8d2b34dc
Nikita Vikulin <nikitavik89@gmail.com>
2023-02-23 11:27:19 +0300

    Добавлен файл conftest.py . Отредактирована фикустура.

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -1,23 +1,14 @@
 from selene.support.shared import browser
 from selene import be, have
-import pytest
-from webdriver_manager.core import driver
 
 
-@pytest.fixture()
-def set_up():
-        driver.set_window_size(800, 600)
-        print("Тест старт")
-        yield
-        print("Окончание теста")
-
-def test_find_google(set_up):
+def test_find_google(window_size):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
-def test_negative_find_google(set_up):
+def test_negative_find_google(window_size):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('geghwheerherhjjjrthjfgnhfgn').press_enter()
     browser.element("//*[@id='topstuff']/div/div/ul/li[1]").should(have.text('Убедитесь, что все слова написаны без ошибок'))
